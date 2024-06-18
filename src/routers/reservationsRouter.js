c648564fda84163dd0745f8dfeb38188712192b5
xszvvfm <bce0102@gmail.com>
2024-06-18 15:56:08 +0900

    Fix: reviews.router.js

--- src/routers/reservationsRouter.js
+++ src/routers/reservationsRouter.js
@@ -6,56 +6,55 @@ import { prisma } from '../utils/prisma.utils.js';
 const reservationsRouter = express.Router();
 
 // 예약 생성 API
-router.post('/', async (req, res) => {
-  const { userId, sitterId, date, service } = req.body;
+// reservationsRouter.post('/', async (req, res) => {
+//   const { userId, sitterId, date, service } = req.body;
 
-  try {
-    const reservation = await prisma.reservation.create({
-      data: {
-        userId: Number(userId),
-        sitterId: Number(sitterId),
-        date: new Date(date),
-        service: service,
-      },
-      include: {
-        User: true,
-        PetSitter: true,
-      },
-    });
+//   try {
+//     const reservation = await prisma.reservation.create({
+//       data: {
+//         userId: Number(userId),
+//         sitterId: Number(sitterId),
+//         date: new Date(date),
+//         service: service,
+//       },
+//       include: {
+//         User: true,
+//         PetSitter: true,
+//       },
+//     });
 
-    res.status(HTTP_STATUS.CREATED).json(reservation);
-  } catch (error) {
-    console.error('예약 생성 실패:', error);
-    res
-      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
-      .json({ error: '예약을 생성할 수 없습니다.' });
-  }
-});
-a;
+//     res.status(HTTP_STATUS.CREATED).json(reservation);
+//   } catch (error) {
+//     console.error('예약 생성 실패:', error);
+//     res
+//       .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+//       .json({ error: '예약을 생성할 수 없습니다.' });
+//   }
+// });
 
 // 예약 조회 API
-router.get('/', async (req, res) => {
-  const { userId } = req.query;
+// reservationsRouter.get('/', async (req, res) => {
+//   const { userId } = req.query;
 
-  try {
-    const reservations = await prisma.reservation.findMany({
-      where: {
-        userId: Number(userId),
-      },
-      include: {
-        User: true,
-        PetSitter: true,
-      },
-    });
+//   try {
+//     const reservations = await prisma.reservation.findMany({
+//       where: {
+//         userId: Number(userId),
+//       },
+//       include: {
+//         User: true,
+//         PetSitter: true,
+//       },
+//     });
 
-    res.status(HTTP_STATUS.OK).json(reservations);
-  } catch (error) {
-    console.error('예약 조회 실패:', error);
-    res
-      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
-      .json({ error: '예약을 조회할 수 없습니다.' });
-  }
-});
+//     res.status(HTTP_STATUS.OK).json(reservations);
+//   } catch (error) {
+//     console.error('예약 조회 실패:', error);
+//     res
+//       .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+//       .json({ error: '예약을 조회할 수 없습니다.' });
+//   }
+// });
 
 /** 예약 삭제 API **/
 reservationsRouter.delete('/:reserveId', async (req, res, next) => {
