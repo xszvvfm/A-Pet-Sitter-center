d4625a7d5231c1062463f9ac848893082f7d4271
gus11als <gus11als>
2024-06-19 20:14:38 +0900

    add: aws-s3를 이용해 펫시터 이미지 업로드작업완료후 병합전커밋  #35

---
+++ multer-s3.config.js
@@ -0,0 +1,26 @@
+import AWS from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import path from 'path';
+import { config } from 'dotenv';
+
+config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET_NAME,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    },
+  }),
+});
+
+export default upload;
