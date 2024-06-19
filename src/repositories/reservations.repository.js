61eec3446320cb6bfc564eefaf7eab2119c97369
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-06-20 00:34:40 +0900

    feat:수정완료

--- src/repositories/reservations.repository.js
+++ src/repositories/reservations.repository.js
@@ -5,21 +5,21 @@ export class ReservationsRepository {
     this.prisma = prisma;
   }
 
-  reservationReadOne = async (userId, id) => {
-    let data = await this.prisma.reservation.findFirst({
-      where: { id: +id, userId: +userId },
-    });
-    data = {
-      user_id: data.userId,
-      sitter_id: data.sitterId,
-      reserve_id: data.reserveId,
-      date: data.date,
-      service_type: data.service_type,
-      created_at: data.createdAt,
-      updated_at: data.updatedAt,
-    };
-    return data;
-  };
+  // reservationReadOne = async (userId, id) => {
+  //   let data = await this.prisma.reservation.findFirst({
+  //     where: { id: +id, userId: +userId },
+  //   });
+  //   data = {
+  //     user_id: data.userId,
+  //     sitter_id: data.sitterId,
+  //     reserve_id: data.reserveId,
+  //     date: data.date,
+  //     service_type: data.service_type,
+  //     created_at: data.createdAt,
+  //     updated_at: data.updatedAt,
+  //   };
+  //   return data;
+  // };
 
   // user_id,
   // sitter_id,
@@ -69,9 +69,9 @@ export class ReservationsRepository {
   };
 
   //상세조회
-  reservationReadOne = async (id) => {
+  reservationReadOne = async (userId, id) => {
     let data = await this.prisma.reservation.findFirst({
-      where: { id: +id },
+      where: { userId: +userId, id: +id },
     });
     data = {
       user_id: data.userId,
