ab590e965819a16e1e06e48b5a98473eaeaf20be
leegilhyeon <164996803+leegilhyeon@users.noreply.github.com>
2024-06-20 12:01:58 +0900

    내정보 이름 수정 api 작성

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -5,7 +5,7 @@ import { requireAccessToken } from '../middlewares/require-access-token.middlewa
 import { prisma } from '../utils/prisma.utils.js';
 
 const userRouter = express.Router();
-
+//내정보 조회
 userRouter.get('/user', requireAccessToken, async (req, res, next) => {
   try {
     const { id } = req.user;
@@ -35,6 +35,56 @@ userRouter.get('/user', requireAccessToken, async (req, res, next) => {
       message: MESSAGES.USER.SUCCEED,
       data,
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//내정보 수정
+userRouter.patch('/user', requireAccessToken, async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const { username } = req.body;
+    if (!username) {
+      return res
+        .status(HTTP_STATUS.UNAUTHORIZED)
+        .json({ message: '수정할 정보를 입력해주세요' });
+    }
+    const user = await prisma.user.findUnique({
+      where: { id: +id },
+    });
+
+    if (username === user.username) {
+      return res
+        .status(400)
+        .json({ message: '동일한 이름입니다. 다시 수정해주세요' });
+    }
+
+    //중복체크
+    const existedName = await prisma.user.findFirst({
+      where: { username },
+    });
+    if (existedName) {
+      return res.status(HTTP_STATUS.CONFLICT).json({
+        message: '이미 존재하는 이름입니다.',
+      });
+    }
+
+    const updateData = { username };
+    const updateUserInfo = await prisma.user.update({
+      where: { id },
+      data: updateData,
+    });
+    const userInfo = {
+      id: updateUserInfo.id,
+      email: updateUserInfo.email,
+      username: updateUserInfo.username,
+      createdAt: updateUserInfo.createdAt,
+      updateData: updateUserInfo.updatedAt,
+    };
+    return res
+      .status(HTTP_STATUS.OK)
+      .json({ message: '정보가 수정되었습니다.', data: userInfo });
   } catch (error) {
     next(error);
   }
